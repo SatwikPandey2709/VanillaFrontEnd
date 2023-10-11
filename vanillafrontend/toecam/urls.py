677688d07b1017890d0a89b522441c02750a316c
SatwikPandey2709 <144765211+SatwikPandey2709@users.noreply.github.com>
2023-10-11 10:28:44 +0530

    Add files via upload

--- vanillafrontend/toecam/urls.py
+++ vanillafrontend/toecam/urls.py
@@ -5,9 +5,16 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('home/'),
-    # path('intro/'),
+    path('home/', views.homepage, name="homepage"),
+    path('welcome/', views.welcome, name="welcome-page"),
     path('login/', views.login, name="login-page"),
     path('signup/', views.signup, name="signup-page"),
-    # path('reset-password/'),
+    path('reset-password/', views.reset, name="reset-password"),
+    path('explore/', views.explore, name="explore"),
+    path('profile/', views.profile, name="profile"),
+    path('messages/', views.messages, name="messages"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path('posts/', views.posts, name="posts"),
+    path('followers/', views.followers, name="followers"),
+    path('following/', views.following, name="following"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- vanillafrontend/toecam/views.py
+++ vanillafrontend/toecam/views.py
@@ -5,3 +5,33 @@ def signup(request):
 
 def login(request):
     return render(request, 'login.html')
+
+def reset(request):
+    return render(request, 'resetpass.html')
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def welcome(request):
+    return render(request, 'intro.html')
+
+def explore(request):
+    return render(request, 'explore.html')
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def messages(request):
+    return render(request, 'messages.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def posts(request):
+    return render(request, 'posts.html')
+
+def followers(request):
+    return render(request, 'followers.html')
+
+def following(request):
+    return render(request, 'following.html')
